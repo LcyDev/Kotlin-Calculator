e5964996c9767a939a7ee80a542d9b66dc15bfeb
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-07-11 15:37:44 -0300

    Create FunctionButton.kt

---
+++ src/main/kotlin/enums/buttons/FunctionButton.kt
@@ -0,0 +1,13 @@
+package io.lwcl.enums.buttons
+
+enum class FunctionButton(val value: String) {
+    LN("ln"),
+    LOG("log"),
+    SIN("sin"),
+    COS("cos"),
+    TAN("tan"),
+    EXP("exp"),
+    ROOT("√"),
+    PI("π"),
+    FACTORIAL("n!");
+}
