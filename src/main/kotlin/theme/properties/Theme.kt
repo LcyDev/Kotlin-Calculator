d41a434fbd53ccb6a2311231e511d8750cc0c095
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-07-11 13:48:27 -0300

    Create Theme.kt

---
+++ src/main/kotlin/theme/properties/Theme.kt
@@ -0,0 +1,11 @@
+package io.lwcl.theme.properties
+
+data class Theme (
+    val name: String,
+    val applicationBackground: String,
+    val textColor: String,
+    val btnEqualTextColor: String,
+    val operatorBackground: String,
+    val numbersBackground: String,
+    val btnEqualBackground: String
+)
