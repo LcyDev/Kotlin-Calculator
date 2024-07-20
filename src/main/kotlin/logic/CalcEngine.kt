f6a0f2331e58993e67f11ed577a1d847af654eeb
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-07-19 21:15:39 -0300

    Update CalcEngine.kt

--- src/main/kotlin/logic/CalcEngine.kt
+++ src/main/kotlin/logic/CalcEngine.kt
@@ -5,7 +5,7 @@ import kotlin.math.pow
 
 object CalcEngine {
 
-    fun performOperation(firstNumber: Double, secondNumber: Double, operator: OperatorButton): Double {
+    fun performOperation(firstNumber: Double, secondNumber: Double, operator: OperatorButton?): Double {
         return when (operator) {
             OperatorButton.ADD -> firstNumber + secondNumber
             OperatorButton.SUBTRACT -> firstNumber - secondNumber
@@ -13,7 +13,7 @@ object CalcEngine {
             OperatorButton.DIVIDE -> if (secondNumber == 0.0) Double.NaN else firstNumber / secondNumber
             OperatorButton.REMAINDER -> firstNumber % secondNumber
             OperatorButton.POWER -> firstNumber.pow(secondNumber)
-            OperatorButton.ROOT -> TODO()
+            else -> 0.0
         }
     }
 }
